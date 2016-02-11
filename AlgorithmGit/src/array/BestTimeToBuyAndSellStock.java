c0b1235eff61708b53c0935b399fe4ab52834546
ratn <ratn@ratn-E7450.internal.synopsys.com>
2016-02-12 03:00:16 +0530

    Adding solution for Best Time to buy and sell stock problem Added solution for stock buy and sell to maximize profit. unlimited buy and sell.

--- AlgorithmGit/src/array/BestTimeToBuyAndSellStock.java
+++ AlgorithmGit/src/array/BestTimeToBuyAndSellStock.java
@@ -5,6 +5,9 @@
  */
 package array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * http://articles.leetcode.com/2010/11/best-time-to-buy-and-sell-stock.html
  * @author ratn
@@ -13,6 +16,7 @@ class Stock {
     public int buy;
     public int sell;
     Stock(){this.buy=0;this.sell=0;}
+    // When stock can buy and sell once.
     Stock getBestTime(Stock stock, int [] stockPrice) {
         int min = 0;
         int max_diff = 0;
@@ -30,12 +34,48 @@ class Stock {
         }
         return stock;
     }
+    void getMaxProfit(int [] stockPrice) {
+        int n = stockPrice.length;
+        if ( n <=1) return;
+        
+        //ArrayList < Stock > stocks = new ArrayList<>();
+        Stock[] stocks = new Stock[n/2];
+        int count=0;int i=0;
+        
+        while( i < n-1 ) {
+            
+            // first go for buying the stock.
+            while ( (i < n-1) && (stockPrice[i+1] <= stockPrice[i]) ) {
+                i++;
+            }
+            if ( i == n-1 ) break;
+            stocks[count] = new Stock();
+            //Stock stock = new Stock();
+            //stocks.add(stock);
+            
+            //stocks.get(count).buy = i++; // time to buy
+            stocks[count].buy = i++;
+            while ( (i < n) && (stockPrice[i] >= stockPrice[i-1] ) ) {
+                i++;
+            }
+            //stocks.get(count).sell = i-1; // time to sell
+            stocks[count].sell = i-1; 
+            count++;
+            
+        }
+        for ( i=0; i< count; i++ ) {
+            //System.out.println("buy : " + stocks.get(i).buy + ", sell : " + stocks.get(i).sell );
+            System.out.println("buy : " + stocks[i].buy + ", sell : " + stocks[i].sell );
+        }
+        
+    }
 }
 public class BestTimeToBuyAndSellStock {
     public static void main(String[] args) {
         int stockPrice [] = {100, 180, 260, 310, 40, 535, 695};
         Stock stock = new Stock();
         stock.getBestTime(stock, stockPrice);
-        System.out.println("buy at : " + stock.buy + ", sell at : " + stock.sell);
+        //System.out.println("buy at : " + stock.buy + ", sell at : " + stock.sell);
+        stock.getMaxProfit(stockPrice);
     }
 }
